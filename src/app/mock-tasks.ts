e236d80ef776eab09d9ae6904f8ac8ca1b98dbc5
ElWas1 <tinoxdwtf@gmail.com>
2022-10-22 19:08:41 -0300

    Agregar mock de tareas y el interface de las mismas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer módulo de Angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: "Agosto 5 a las 17:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Bootstrap",
+        day: "Agosto 5 a las 19:00",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer mi libro favorito",
+        day: "Agosto 5 a las 21:00",
+        reminder: false
+    }
+]
